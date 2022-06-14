ef55067683c01ff486e73727b07f54f328eb678d
Daegudude <>
2022-06-14 22:37:18 +0900

    Update: Add test cases for html-text-converter

--- racing-car-katas/text-converter/html-text-convert.test.js
+++ racing-car-katas/text-converter/html-text-convert.test.js
@@ -1,11 +1,54 @@
-var HtmlTextConverter = require('../text-converter/html-text-converter.js');
+const fs = require("fs");
+var HtmlTextConverter = require("../text-converter/html-text-converter.js");
 
-describe('Html Converter', function() {
-	describe('HtmlTextConverter', function() {
-		it('file name works well', function() {
-			var converter = new HtmlTextConverter('foo');
-      expect(converter.getFilename()).toBe('foo')
-		});
-	});
+describe("Html Converter", function () {
+  describe("HtmlTextConverter", function () {
+    it("file name works well", function () {
+      var converter = new HtmlTextConverter("/foo.txt");
+      expect(converter.getFilename()).toBe("/foo.txt");
+      converter.convertToHtml();
+    });
 
+    it("converts '&' to '&amp;'", function () {
+      var converter = new HtmlTextConverter("/ampersand.txt");
+      expect(converter.convertToHtml()).toBe(
+        "Tom &amp; Jerry was one of my favorite tv show when I was young."
+      );
+    });
+
+    it("converts '<' to '&lt;'", function () {
+      var converter = new HtmlTextConverter("/lessThan.txt");
+
+      expect(converter.convertToHtml()).toBe(
+        "What does this will yield in javascript? '0 &lt; 3'"
+      );
+    });
+
+    it("converts '>' to '&gt;'", function () {
+      var converter = new HtmlTextConverter("/greaterThan.txt");
+
+      expect(converter.convertToHtml()).toBe(
+        "What does this will yield in javascript? '5 &gt; 7'"
+      );
+    });
+
+    it("converts the new line well", function () {
+      var converter = new HtmlTextConverter("/newLine.txt");
+
+      expect(converter.convertToHtml()).toBe("Ahoi!<br />I am Daegudude.");
+    });
+
+    it("Normal text isn't converted to anything", function () {
+      var converter = new HtmlTextConverter("/normal.txt");
+
+      expect(converter.convertToHtml()).toBe("We are refactoring book club.");
+    });
+  });
+
+  describe("HtmlTextConverter works well", function () {
+    var converter = new HtmlTextConverter("/foo.txt");
+    expect(converter.convertToHtml()).toBe(
+      "Hello there, Let's do some javascript exercise.<br /><br />What will these yield in javascript? '5 &gt; 7', '0 &lt; 3<br /><br />Have you heard of bitwise AND('&amp;')?"
+    );
+  });
 });
--- racing-car-katas/text-converter/html-text-converter.js
+++ racing-car-katas/text-converter/html-text-converter.js
@@ -1,68 +1,66 @@
-var fs = require('fs');
+var fs = require("fs");
 
-HtmlTextConverter = function(fullFilenameWithPath) {
-	this._fullFilenameWithPath = fullFilenameWithPath;
+HtmlTextConverter = function (fullFilenameWithPath) {
+  this._fullFilenameWithPath = fullFilenameWithPath;
 };
 
 HtmlTextConverter.prototype = {
+  convertToHtml: function () {
+    var text = fs
+      .readFileSync(`${__dirname}${this._fullFilenameWithPath}`)
+      .toString();
 
-	convertToHtml: function() {
+    var stashNextCharacterAndAdvanceThePointer = function () {
+      var c = text.charAt(i);
+      i += 1;
+      return c;
+    };
 
-		var text = fs.readFileSync(this._fullFilenameWithPath).toString();
+    var addANewLine = function () {
+      var line = convertedLine.join("");
+      html.push(line);
+      convertedLine = [];
+    };
 
-		var stashNextCharacterAndAdvanceThePointer = function() {
-			var c = text.charAt(i);
-			i += 1;
-			return c;
-		};
+    var pushACharacterToTheOutput = function () {
+      convertedLine.push(characterToConvert);
+    };
 
-		var addANewLine = function() {
-			var line = convertedLine.join('');
-			html.push(line);
-			convertedLine = [];
-		};
+    var i = 0;
+    var html = [];
+    var convertedLine = [];
+    var characterToConvert = stashNextCharacterAndAdvanceThePointer();
 
-		var pushACharacterToTheOutput = function() {
-			convertedLine.push(characterToConvert);
-		};
+    while (i <= text.length) {
+      switch (characterToConvert) {
+        case "<":
+          convertedLine.push("&lt;");
+          break;
+        case ">":
+          convertedLine.push("&gt;");
+          break;
+        case "&":
+          convertedLine.push("&amp;");
+          break;
+        case "\n":
+          addANewLine();
+          break;
+        default:
+          pushACharacterToTheOutput();
+      }
 
-		var i = 0;
-		var html = [];
-		var convertedLine = [];
-		var characterToConvert = stashNextCharacterAndAdvanceThePointer();
-		while (i <= text.length) {
+      characterToConvert = stashNextCharacterAndAdvanceThePointer();
+    }
 
-			switch (characterToConvert) {
-				case '<':
-					convertedLine.push('&lt;');
-					break;
-				case '>':
-					convertedLine.push('&gt;');
-					break;
-				case '&':
-					convertedLine.push('&amp;');
-					break;
-				case '\n':
-					addANewLine();
-					break;
-				default:
-					pushACharacterToTheOutput();
-			}
+    addANewLine();
+    return html.join("<br />");
+  },
 
-			characterToConvert = stashNextCharacterAndAdvanceThePointer();
-		}
-
-		addANewLine();
-		return html.join('<br />');
-	},
-
-	getFilename: function() {
-		return this._fullFilenameWithPath;
-	}
+  getFilename: function () {
+    return this._fullFilenameWithPath;
+  },
 };
 
-var converter = new HtmlTextConverter('foo');
-console.log(converter.getFilename())
-console.log(converter.convertToHtml())
+fs.writeFileSync("foo.txt", "This writes well");
 
 module.exports = HtmlTextConverter;
