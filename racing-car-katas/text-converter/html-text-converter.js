43e409bd14f095b816a38309acabee3404a2fb6f
Daegudude <>
2022-06-15 18:31:50 +0900

    Add increaseTextContentIndex

--- racing-car-katas/text-converter/html-text-converter.js
+++ racing-car-katas/text-converter/html-text-converter.js
@@ -1,19 +1,95 @@
 var fs = require("fs");
 
-HtmlTextConverter = function (fullFilenameWithPath) {
-  this._fullFilenameWithPath = fullFilenameWithPath;
-};
+// HtmlTextConverter = function (fullFilenameWithPath) {
+//   this._fullFilenameWithPath = fullFilenameWithPath;
+// };
 
-HtmlTextConverter.prototype = {
-  convertToHtml: function () {
-    var text = fs
+// First, I need to know how program does work....
+// Write a good name so I can abstract?
+
+// I want to first tackle good names..
+
+// HtmlTextConverter.prototype = {
+
+//   convertToHtml: function () {
+//     const textContent = fs
+//       .readFileSync(`${__dirname}${this._fullFilenameWithPath}`)
+//       .toString();
+
+//     const stashNextCharacterAndAdvanceThePointer = function () {
+//       const character = textContent.charAt(textContentIndex);
+//       textContentIndex += 1;
+//       return character;
+//     };
+
+//     var addANewLine = function () {
+//       var line = convertedLine.join("");
+//       html.push(line);
+//       convertedLine = [];
+//     };
+
+//     var pushACharacterToTheOutput = function () {
+//       convertedLine.push(characterToConvert);
+//     };
+
+//     var textContentIndex = 0;
+//     var html = [];
+//     var convertedLine = [];
+//     var characterToConvert = stashNextCharacterAndAdvanceThePointer();
+
+//     while (textContentIndex <= textContent.length) {
+//       switch (characterToConvert) {
+//         case "<":
+//           convertedLine.push("&lt;");
+//           break;
+//         case ">":
+//           convertedLine.push("&gt;");
+//           break;
+//         case "&":
+//           convertedLine.push("&amp;");
+//           break;
+//         case "\n":
+//           addANewLine();
+//           break;
+//         default:
+//           pushACharacterToTheOutput();
+//       }
+
+//       characterToConvert = stashNextCharacterAndAdvanceThePointer();
+//     }
+
+//     addANewLine();
+
+//     console.log({ textContentIndex, html, convertedLine, characterToConvert });
+
+//     console.log("joined html: ", html.join("<br />"));
+//     return html.join("<br />");
+//   },
+
+//   getFilename: function () {
+//     return this._fullFilenameWithPath;
+//   },
+// };
+
+class HtmlTextConverter {
+  constructor(fullFilenameWithPath) {
+    this._fullFilenameWithPath = fullFilenameWithPath;
+    this.textContentIndex = 0;
+  }
+
+  increaseTextContentIndex() {
+    this.textContentIndex++;
+  }
+
+  convertToHtml() {
+    const textContent = fs
       .readFileSync(`${__dirname}${this._fullFilenameWithPath}`)
       .toString();
 
-    var stashNextCharacterAndAdvanceThePointer = function () {
-      var c = text.charAt(i);
-      i += 1;
-      return c;
+    const stashNextCharacterAndAdvanceThePointer = () => {
+      const character = textContent.charAt(this.textContentIndex);
+      this.increaseTextContentIndex();
+      return character;
     };
 
     var addANewLine = function () {
@@ -26,12 +102,11 @@ HtmlTextConverter.prototype = {
       convertedLine.push(characterToConvert);
     };
 
-    var i = 0;
     var html = [];
     var convertedLine = [];
     var characterToConvert = stashNextCharacterAndAdvanceThePointer();
 
-    while (i <= text.length) {
+    while (this.textContentIndex <= textContent.length) {
       switch (characterToConvert) {
         case "<":
           convertedLine.push("&lt;");
@@ -53,13 +128,18 @@ HtmlTextConverter.prototype = {
     }
 
     addANewLine();
-    return html.join("<br />");
-  },
 
-  getFilename: function () {
+    console.log({ html, convertedLine, characterToConvert });
+    console.log("this.textContentIndex", this.textContentIndex);
+
+    console.log("joined html: ", html.join("<br />"));
+    return html.join("<br />");
+  }
+
+  getFilename() {
     return this._fullFilenameWithPath;
-  },
-};
+  }
+}
 
 fs.writeFileSync("foo.txt", "This writes well");
 
